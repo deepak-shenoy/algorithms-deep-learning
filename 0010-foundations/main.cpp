d3e239a734ec5486301ec2f7721b2f70063e9f22
Deepak Shenoy <deepak@local>
2026-01-11 23:49:54 -0500

    First draft

--- 0010-foundations/main.cpp
+++ 0010-foundations/main.cpp
@@ -9,5 +9,22 @@
 #include <functional>
 
 int main() {
+
+    std::vector<std::function<bool(double, double)>> comparators {
+        std::less<double>(),
+        std::less_equal<double>(),
+        std::greater<double>(),
+        std::greater_equal<double>()
+    };
+
+    double x = 10.;
+    double y = 10.;
+
+    auto compare = [&x, &y] (const std::function<bool(double, double)> &comparator) {
+        bool b = comparator(x, y);
+        std::cout << (b?"TRUE": "FALSE") << "\n";
+    };
+
+    std::for_each(comparators.begin(), comparators.end(), compare);
     return 0;
 }
